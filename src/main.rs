9c32fb034536b18697a4145a7df32f94b16f0527
Johan Liebert <bggbss@rambler.ru>
2023-08-19 03:25:44 +0300

    All of slices

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-fn first_word(s: &String) -> &str {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return &s[..i];
+            return &s[0..i];
         }
     }
 
@@ -11,7 +11,28 @@ fn first_word(s: &String) -> &str {
 }
 
 fn main() {
-    let text = String::from("hello world");
-    let first_word = first_word(&text);
-    println!("{}",first_word);
+    let my_string = String::from("hello world");
+
+    // `first_word` работает с фрагментами `String`, как частичными, так и целыми
+    let word = first_word(&my_string[0..6]);
+    println!("{word}");
+    let word = first_word(&my_string[..]);
+    println!("{word}");
+    // `first_word` также работает со ссылками на `String`, которые эквивалентны
+    // к целым фрагментам `String`.
+    let word = first_word(&my_string);
+    println!("{word}");
+
+    let my_string_literal = "hello world";
+
+    // `first_word` works on slices of string literals, whether partial or whole
+    let word = first_word(&my_string_literal[0..6]);
+    println!("{word}");
+    let word = first_word(&my_string_literal[..]);
+    println!("{word}");
+
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let word = first_word(my_string_literal);
+    println!("{word}");
 }
